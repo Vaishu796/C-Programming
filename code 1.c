d348b63e88268b987f88bb2c264a1c8b083b6431
Vaishali Bhatt <vaishalibhatt46@gmail.com>
2024-10-03 00:46:19 -0700

    Add files via upload

---
+++ code 1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c;
+printf("entre the first number");
+scanf("%d",&a);
+
+printf("entre the second number");
+scanf("%d",&b);
+
+c=a;
+a=b;
+b=c;
+
+printf("value of a%d",a);
+printf("value of b%d",b);
+
+return 0;
+
+
+
+
+
+
+
+
+}
